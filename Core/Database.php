3f65a861c9759c1ea601711725b3dd460a0cc8ce
Zorehi <jeremy284@hotmail.com>
2022-09-19 18:39:43 +0200

    Initial commit

---
+++ Core/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database extends PDO {
+    // Instance unique de la classe
+    private static $instance;
+
+    // Propriétés de la base de données
+    private const DBHOST = "77.196.65.135";
+    private const DBNAME = "CSGOTournamenentMaker";
+    private const DBUSER = "admin";
+    private const DBPASS = "valentin";
+
+    private function __construct() {
+        // DSN de connexion
+        $_dsn = "mysql:host=" . self::DBHOST . ";dbname=" . self::DBNAME;
+
+        try {
+            parent::__construct($_dsn, self::DBUSER, self::DBPASS);
+
+            $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+?>
---
+++ Models/Model.php
@@ -0,0 +1,156 @@
+<?php
+    namespace App\Models;
+
+    use App\Core\Database;
+
+    class Model extends Database {
+        protected $table;
+        protected $idName = "";
+
+        private $db;
+
+        /**
+         * Sélection de tous les enregistrements d'une table
+         * @return array Tableau des enregistrements trouvés
+         */
+        public function findAll()
+        {
+            $query = $this->requete('SELECT * FROM '.$this->table);
+            return $query->fetchAll();
+        }
+
+        /**
+         * Sélection de plusieurs enregistrements suivant un tableau de critères
+         * @param array $criteres Tableau de critères
+         * @return array Tableau des enregistrements trouvés
+         */
+        public function findBy(array $criteres)
+        {
+            $champs = [];
+            $valeurs = [];
+
+            // On boucle pour "éclater le tableau"
+            foreach($criteres as $champ => $valeur) {
+                $champs[] = "$champ = ?";
+                $valeurs[]= $valeur;
+            }
+
+            // On transforme le tableau en chaîne de caractères séparée par des AND
+            $liste_champs = implode(' AND ', $champs);
+
+            // On exécute la requête
+            return $this->requete("SELECT * FROM {$this->table} WHERE $liste_champs", $valeurs)->fetchAll();
+        }
+
+        /**
+         * Sélection d'un enregistrements suivant l'id
+         * @param array $criteres Tableau de critères
+         * @return array Tableau des enregistrements trouvés
+         */
+        public function findById(int $id)
+        {
+            // On exécute la requête
+            return $this->requete("SELECT * FROM {$this->table} WHERE ". $this->idName ." = {$id}")->fetch();
+        }
+
+
+        /**
+         * Insertion d'un enregistrement suivant un tableau de données
+         * @return bool
+         */
+        public function create()
+        {
+            $champs = [];
+            $inter = [];
+            $valeurs = [];
+
+            // On boucle pour éclater le tableau
+            foreach($this as $champ => $valeur) {
+                // INSERT INTO annonces (titre, description, actif) VALUES (?, ?, ?)
+                if($valeur !== null && $champ != 'db' && $champ != 'table' && $champ != 'idName') {
+                    $champs[] = $champ;
+                    $inter[] = "?";
+                    $valeurs[] = $valeur;
+                }
+            }
+
+            // On transforme le tableau "champs" en une chaine de caractères
+            $liste_champs = implode(', ', $champs);
+            $liste_inter = implode(', ', $inter);
+
+            // On exécute la requête
+            return $this->requete('INSERT INTO '.$this->table.' ('. $liste_champs.')VALUES('.$liste_inter.')', $valeurs);
+        }
+
+        /**
+         * Mise à jour d'un enregistrement suivant un tableau de données
+         * @param int $id id de l'enregistrement à modifier
+         * @param Model $model Objet à modifier
+         * @return bool
+         */
+        public function update()
+        {
+            $champs = [];
+            $valeurs = [];
+
+            // On boucle pour éclater le tableau
+            foreach($this as $champ => $valeur) {
+                // UPDATE annonces SET titre = ?, description = ?, actif = ? WHERE id= ?
+                if($valeur !== null && $champ != 'db' && $champ != 'table'){
+                    $champs[] = "$champ = ?";
+                    $valeurs[] = $valeur;
+                }
+            }
+            $valeurs[] = $this->$this->idName;
+
+            // On transforme le tableau "champs" en une chaine de caractères
+            $liste_champs = implode(', ', $champs);
+
+            // On exécute la requête
+            return $this->requete('UPDATE '.$this->table.' SET '. $liste_champs.' WHERE '. $this->idName .' = ?', $valeurs);
+        }
+
+        /**
+         * Suppression d'un enregistrement
+         * @param int $id id de l'enregistrement à supprimer
+         * @return bool 
+         */
+        public function delete(int $id) 
+        {
+            return $this->requete("DELETE FROM {$this->table} WHERE '. $this->idName .' = ?", [$id]);
+        }
+
+        public function requete(string $sql, array $attributs = null)
+        {
+            $this->db = Database::getInstance();
+
+            if ($attributs !== null) {
+                $query = $this->db->prepare($sql);
+                $query->execute($attributs);
+                return $query;
+            } else {
+                return $this->db->query($sql);
+            }
+        }
+
+        /**
+         * Hydratation des données
+         * @param array $donnees Tableau associatif des données
+         * @return self Retourne l'objet hydraté
+         */
+        public function hydrate(array $donnees)
+        {
+            foreach ($donnees as $key => $value) {
+                // On récupère le nom du setter correspondant à l'attribut.
+                $method = 'set'.ucfirst($key);
+                
+                // Si le setter correspondant existe.
+                if (method_exists($this, $method)) {
+                    // On appelle le setter.
+                    $this->$method($value);
+                }
+            }
+            return $this;
+        }
+    }
+?>
