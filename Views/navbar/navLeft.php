97bfde96e10480ce712e35d03ee282fd4641b5ce
Zorehi <jeremy284@hotmail.com>
2022-10-23 23:33:38 +0200

    Ajout des barres de navigation

---
+++ Views/navbar/navLeft.php
@@ -0,0 +1,16 @@
+<div class="navLeft">
+    <div class="navLeft__head" id="navleft">
+        <a href="/" class="selected">
+            <div></div>
+            <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yG/r/YHLvfJvVfG6.png" style="background-position:0 0;background-size:auto;width:20px;height:20px;background-repeat:no-repeat;display:inline-block">
+            <span>Accueil</span>
+            <div></div>
+        </a>
+        <a href="">
+            <div></div>
+            <img src="https://scontent.fbod1-1.fna.fbcdn.net/v/t39.30808-1/290213391_2326193007536388_4642470361414569553_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=109&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=ubmamYjOsXUAX_b5FKA&_nc_ht=scontent.fbod1-1.fna&oh=00_AT-EXu8Qo-91XzEHhMtz1DkE1nw4aoRyDjs82qjME9JGog&oe=6355FF76" style="height: 24px;width: 24px;border-radius: 50%;">
+            <span style="margin-left: 12px;"><?= $_SESSION["user"]["prenom"] . " " . $_SESSION["user"]["nom"] ?></span>
+            <div></div>
+        </a>
+    </div>
+</div>
