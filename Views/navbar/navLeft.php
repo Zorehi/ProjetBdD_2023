4049db0eee46e4d29ce98d7c3bd004e92c320de0
Zorehi <jeremy284@hotmail.com>
2022-10-28 00:40:56 +0200

    Ajout du panel houses

--- Views/navbar/navLeft.php
+++ Views/navbar/navLeft.php
@@ -1,16 +1,133 @@
-<div class="navLeft">
-    <div class="navLeft__head" id="navleft">
-        <a href="/" class="selected">
+<div class="navLeft extended" id="navLeft">
+    <div class="navLeft__head">
+        <a href="/" class="selected navLeft__button">
             <div></div>
-            <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yG/r/YHLvfJvVfG6.png" style="background-position:0 0;background-size:auto;width:20px;height:20px;background-repeat:no-repeat;display:inline-block">
+            <i style="background-image: url(https://static.xx.fbcdn.net/rsrc.php/v3/yG/r/YHLvfJvVfG6.png);background-position:0 0;background-size:auto;width:20px;height:20px;background-repeat:no-repeat;display:inline-block"></i>
             <span>Accueil</span>
             <div></div>
         </a>
-        <a href="">
+        <a href="" class="navLeft__button">
             <div></div>
-            <img src="https://scontent.fbod1-1.fna.fbcdn.net/v/t39.30808-1/290213391_2326193007536388_4642470361414569553_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=109&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=ubmamYjOsXUAX_b5FKA&_nc_ht=scontent.fbod1-1.fna&oh=00_AT-EXu8Qo-91XzEHhMtz1DkE1nw4aoRyDjs82qjME9JGog&oe=6355FF76" style="height: 24px;width: 24px;border-radius: 50%;">
+            <img src="https://scontent.fbod1-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=D4MkQJTW3IMAX9AQGAE&_nc_ht=scontent.fbod1-1.fna&oh=00_AT9MhxjvbOepN3BUs7ycUyYNL0ZFUPSJN4QuN_ZmovoBdw&oe=6364B778" style="height: 24px;width: 24px;border-radius: 50%;">
             <span style="margin-left: 12px;"><?= $_SESSION["user"]["prenom"] . " " . $_SESSION["user"]["nom"] ?></span>
             <div></div>
         </a>
+    </div>
+    <div class="navLeft__houses">
+        <div class="navLeft__button" id="Maisons">
+            <div></div>
+            <div class="icon">
+                <i class="fa-solid fa-building fa-lg" style="background-position:0 0;background-size:auto;background-repeat:no-repeat;display:inline-block"></i>
+            </div>
+            <span>Maisons</span>
+            <div></div>
+        </div>
+    </div>
+</div>
+<div id="panel" style="visibility: hidden;">
+    <div class="navLeft-border"></div>
+    <div class="navLeft-cloud"></div>
+    <div class="housesPanel" id="housesPanel">
+        <div class="housesPanel__title">
+            <h1>Maisons</h1>
+        </div>
+        <div>
+            <div class="housesPanel__search">
+                <label for="search_houses">
+                    <div>
+                        <svg fill="currentColor" viewBox="0 0 16 16" width="1em" height="1em">
+                            <g fill-rule="evenodd" transform="translate(-448 -544)">
+                                <g fill-rule="nonzero">
+                                    <path d="M10.743 2.257a6 6 0 1 1-8.485 8.486 6 6 0 0 1 8.485-8.486zm-1.06 1.06a4.5 4.5 0 1 0-6.365 6.364 4.5 4.5 0 0 0 6.364-6.363z" transform="translate(448 544)"></path>
+                                    <path d="M10.39 8.75a2.94 2.94 0 0 0-.199.432c-.155.417-.23.849-.172 1.284.055.415.232.794.54 1.103a.75.75 0 0 0 1.112-1.004l-.051-.057a.39.39 0 0 1-.114-.24c-.021-.155.014-.356.09-.563.031-.081.06-.145.08-.182l.012-.022a.75.75 0 1 0-1.299-.752z" transform="translate(448 544)"></path>
+                                    <path d="M9.557 11.659c.038-.018.09-.04.15-.064.207-.077.408-.112.562-.092.08.01.143.034.198.077l.041.036a.75.75 0 0 0 1.06-1.06 1.881 1.881 0 0 0-1.103-.54c-.435-.058-.867.018-1.284.175-.189.07-.336.143-.433.2a.75.75 0 0 0 .624 1.356l.066-.027.12-.061z" transform="translate(448 544)"></path>
+                                    <path d="m13.463 15.142-.04-.044-3.574-4.192c-.599-.703.355-1.656 1.058-1.057l4.191 3.574.044.04c.058.059.122.137.182.24.249.425.249.96-.154 1.41l-.057.057c-.45.403-.986.403-1.411.154a1.182 1.182 0 0 1-.24-.182zm.617-.616.444-.444a.31.31 0 0 0-.063-.052c-.093-.055-.263-.055-.35.024l.208.232.207-.206.006.007-.22.257-.026-.024.033-.034.025.027-.257.22-.007-.007zm-.027-.415c-.078.088-.078.257-.023.35a.31.31 0 0 0 .051.063l.205-.204-.233-.209z" transform="translate(448 544)"></path>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <input type="text" id="search_houses" placeholder="Rechercher des Maisons">
+                </label>
+            </div>
+            <div>
+                <div class="housesPanel__search-button display-none" id="searchGlobalContainer">
+                    <a class="housesPanel__button" href="#" id="searchHousesGlobal">
+                        <div class="img">
+                            <i data-visualcompletion="css-img" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/y2/r/QouyfNuGrAZ.png&quot;); background-position: -170px -187px; background-size: auto; width: 16px; height: 16px; background-repeat: no-repeat; display: inline-block;"></i>
+                        </div>
+                        <div class="text">
+                            <span>Rechercher <strong></strong> dans les maisons</span>
+                        </div>
+                        <div class="opacity-0 background-hover"></div>
+                    </a>
+                </div>
+                <div class="housesPanel__create display-true" id="createContainer">
+                    <a href="/houses/create" id="btnCreateHouse">
+                        <span>
+                            <i data-visualcompletion="css-img" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yT/r/i14dpO3gzZZ.png&quot;); background-position: 0px -1659px; background-size: auto; width: 16px; height: 16px; background-repeat: no-repeat; display: inline-block;"></i>
+                            <span>Créer une nouvelle maison</span>
+                        </span>
+                        <div class="background-hover opacity-0"></div>
+                    </a>
+                </div>
+                <div class="housesPanel__separator display-true" id="separator"></div>
+                <div class="housesPanel__show">
+                    <div id="pinHouses" class="display-true">
+                        <span>Epinglés</span>
+                        <div>
+                            <a class="housesPanel__button" href="#">
+                                <div class="img">
+                                    <svg aria-hidden="true" class="" data-visualcompletion="ignore-dynamic" role="none" style="height: 36px; width: 36px;">
+                                        <mask id="jsc_c_34x">
+                                            <rect cy="18" fill="white" height="36" rx="8" ry="8" width="36" x="0" y="0"></rect>
+                                        </mask>
+                                        <g mask="url(#jsc_c_34x)">
+                                            <image x="0" y="0" style="width: 36px;height: 36px;" xlink:href="https://scontent.fbod1-1.fna.fbcdn.net/v/t1.6435-9/42087349_1865028706923568_966827946130014208_n.jpg?stp=c12.0.50.50a_cp0_dst-jpg_p50x50&amp;_nc_cat=100&amp;ccb=1-7&amp;_nc_sid=70495d&amp;_nc_ohc=j9Ie4DhMcswAX8c63W3&amp;_nc_ht=scontent.fbod1-1.fna&amp;oh=00_AT-dDh0bys5IxfaCbVQ1w004BvdRsCANmJuB-YCYlw-5Tg&amp;oe=637B716D" style="height: 36px; width: 36px;"></image>
+                                            <rect class="" cy="18" fill="none" height="36" rx="8" ry="8" width="36" x="0" y="0"></rect>
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div class="text">
+                                    <span>Pharmatech</span>
+                                    <span>En ligne il y a 6 heures</span>
+                                </div>
+                                <div class="pin-icon">
+                                    <i data-visualcompletion="css-img" class="" style="background-image: url(https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/XwHPmbqCLP0.png);background-position: 0px -373px; background-size: auto; width: 16px; height: 16px; background-repeat: no-repeat; display: inline-block;"></i>
+                                    <div class="opacity-0 background-hover"></div>
+                                </div>
+                                <div class="opacity-0 background-hover"></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="myHouses" class="display-true"> 
+                        <span>Maisons dont vous êtes propriétaire</span>
+                        <div>
+                            <a class="housesPanel__button" href="#">
+                                <div class="img">
+                                    <svg style="height: 36px; width: 36px;">
+                                        <mask id="jsc_c_35x">
+                                            <rect cy="18" fill="white" height="36" rx="8" ry="8" width="36" x="0" y="0"></rect>
+                                        </mask>
+                                        <g mask="url(#jsc_c_35x)">
+                                            <image style="width: 36px;height: 36px;" xlink:href="https://scontent.fbod1-1.fna.fbcdn.net/v/t1.6435-9/42087349_1865028706923568_966827946130014208_n.jpg?stp=c12.0.50.50a_cp0_dst-jpg_p50x50&amp;_nc_cat=100&amp;ccb=1-7&amp;_nc_sid=70495d&amp;_nc_ohc=j9Ie4DhMcswAX8c63W3&amp;_nc_ht=scontent.fbod1-1.fna&amp;oh=00_AT-dDh0bys5IxfaCbVQ1w004BvdRsCANmJuB-YCYlw-5Tg&amp;oe=637B716D" style="height: 36px; width: 36px;"></image>
+                                            <rect class="" cy="18" fill="none" height="36" rx="8" ry="8" width="36" x="0" y="0"></rect>
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div class="text">
+                                    <span>Pharmatech</span>
+                                    <span>En ligne il y a 6 heures</span>
+                                </div>
+                                <div class="pin-icon">
+                                    <i data-visualcompletion="css-img" class="" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yT/r/lM3wJmRoC7j.png&quot;); background-position: 0px -38px; background-size: auto; width: 16px; height: 16px; background-repeat: no-repeat; display: inline-block;"></i>
+                                    <div class="opacity-0 background-hover"></div>
+                                </div>
+                                <div class="opacity-0 background-hover"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- js/navLeft.js
+++ js/navLeft.js
@@ -1,11 +1,244 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    navbar = document.getElementById("navleft");
-    for (var i = 0; i < navbar.children.length; i++) {
-        navbar.children[i].addEventListener("mouseenter", function(event) {
-            event.target.children[3].style.opacity = "1";
-        })
-        navbar.children[i].addEventListener("mouseleave", function(event) {
-            event.target.children[3].style.opacity = "0";
-        })
+const navbar_buttons = document.getElementsByClassName("navLeft__button");
+const btnHouses = document.getElementById("Maisons");
+const navLeft = document.getElementById("navLeft");
+const panel = document.getElementById("panel");
+const housesPanel = document.getElementById("housesPanel");
+const btnCreateHouse = document.getElementById("btnCreateHouse");
+const search_houses = document.getElementById("search_houses");
+const searchHousesGlobal = document.getElementById("searchHousesGlobal");
+const myHouses = document.getElementById("myHouses");
+const pinHouses = document.getElementById("pinHouses");
+const separator = document.getElementById("separator");
+const searchGlobalContainer = document.getElementById("searchGlobalContainer");
+const createContainer = document.getElementById("createContainer");
+const housesPanel_buttons = document.getElementsByClassName("housesPanel__button");
+const pin_icons = document.getElementsByClassName("pin-icon");
+
+for (var i = 0; i < navbar_buttons.length; i++) {
+    navbar_buttons[i].addEventListener("mouseenter", function(event) { event.target.children[3].style.opacity = "1"; });
+    navbar_buttons[i].addEventListener("mouseleave", function(event) { event.target.children[3].style.opacity = "0"; });
+}
+
+/**
+ * Permet de savoir si un element est affiché
+ * 
+ * @param {HTMLElement} elem Element HTML à tester
+ * @returns Booleen
+ */
+const isVisible = elem => elem.style.visibility == "visible" ? true : false;
+
+/**
+ * Permet de cacher le menu en cliquant à coté
+ * 
+ * @param {Event} event 
+ */
+const outsideClickListener = event => {
+    if (!btnHouses.contains(event.target) && !housesPanel.contains(event.target) && isVisible(panel)) {
+        panel.style.visibility = "hidden";
+        for (let button of navbar_buttons) {
+            button.classList.remove("selected");
+            button.children[2].style.display = "block";
+        }
+        navLeft.classList.replace("small", "extended");
+        navbar_buttons[0].classList.add("selected");
+        btnHouses.children[3].style.borderRadius = "";
+        btnHouses.children[3].style.inset = "";
+
+        search_houses.value = "";
+        hideSearch();
+
+        document.removeEventListener("click", outsideClickListener);
+        removeAllEventForPanelHouses();
     }
+}
+
+btnHouses.addEventListener("click", function(event) {
+    if (isVisible(panel)) {
+        document.dispatchEvent(new Event("click"));
+        return;
+    }
+    document.addEventListener("click", outsideClickListener);
+    for (var i = 0; i < navbar_buttons.length; i++) {
+        navbar_buttons[i].classList.remove("selected");
+        navbar_buttons[i].children[2].style.display = "none";
+    }
+    this.children[3].style.borderRadius = "16px";
+    this.children[3].style.inset = "4px 14px";
+    this.classList.add("selected");
+    navLeft.classList.replace("extended", "small");
+    panel.style.visibility = "visible";
+
+    addAllEventForPanelHouses();
 })
+
+
+/**
+ * Ajoute tous les évenement liés au panel maisons
+ */
+const addAllEventForPanelHouses = function() {
+    btnCreateHouse.addEventListener("mouseup", animateBtnCreateHouse);
+    btnCreateHouse.addEventListener("mouseleave", animateBtnCreateHouse);
+    btnCreateHouse.addEventListener("mouseenter", animateBtnCreateHouse);
+    btnCreateHouse.addEventListener("mousedown", animateBtnCreateHouse);
+
+    search_houses.addEventListener("keydown", animateSearch);
+    
+    for (let button of housesPanel_buttons) {
+        button.addEventListener("mouseenter", animateButtons);
+        button.addEventListener("mouseleave", animateButtons);
+        button.addEventListener("mousedown", animateButtons);
+        button.addEventListener("mouseup", animateButtons);
+    }
+    for (let pin_icon of pin_icons) {
+        pin_icon.addEventListener("mouseenter", animatePinIcon);
+        pin_icon.addEventListener("mouseleave", animatePinIcon);
+        pin_icon.addEventListener("mousedown", animatePinIcon);
+        pin_icon.addEventListener("mouseup", animatePinIcon);
+    }
+}
+
+/**
+ * Enlève tous les évenement liés au panel maisons
+ */
+const removeAllEventForPanelHouses = function() {
+    btnCreateHouse.removeEventListener("mouseup", animateBtnCreateHouse);
+    btnCreateHouse.removeEventListener("mouseleave", animateBtnCreateHouse);
+    btnCreateHouse.removeEventListener("mouseenter", animateBtnCreateHouse);
+    btnCreateHouse.removeEventListener("mousedown", animateBtnCreateHouse);
+    
+    search_houses.removeEventListener("keydown", animateSearch);
+    
+    for (let button of housesPanel_buttons) {
+        button.removeEventListener("mouseenter", animateButtons);
+        button.removeEventListener("mouseleave", animateButtons);
+        button.removeEventListener("mousedown", animateButtons);
+        button.removeEventListener("mouseup", animateButtons);
+    }
+    for (let pin_icon of pin_icons) {
+        pin_icon.removeEventListener("mouseenter", animatePinIcon);
+        pin_icon.removeEventListener("mouseleave", animatePinIcon);
+        pin_icon.removeEventListener("mousedown", animatePinIcon);
+        pin_icon.removeEventListener("mouseup", animatePinIcon);
+    }
+}
+
+/**
+ * Permet le hover des buttons
+ * 
+ * @param {Event} event 
+ */
+const animateButtons = function(event) {
+    switch (event.type) {
+        case "mouseenter":
+            this.children[this.children.length-1].classList.replace("opacity-0", "opacity-1");
+            if (this.id != "searchHousesGlobal") this.children[2].children[1].classList.replace("opacity-1", "opacity-0");
+            break;
+        case "mouseleave":
+            this.children[this.children.length-1].classList.replace("opacity-1", "opacity-0");
+        case "mouseup":
+            this.children[this.children.length-1].classList.replace("background-pressed", "background-hover");
+            break;
+        case "mousedown":
+            this.children[this.children.length-1].classList.replace("background-hover", "background-pressed");
+            break;
+    }
+}
+
+/**
+ * Permet d'animer les icon épingler sur les buttons 
+ * 
+ * @param {Event} event 
+ */
+const animatePinIcon = function(event) {
+    switch (event.type) {
+        case "mouseenter":
+            this.children[1].classList.replace("opacity-0", "opacity-1");
+            this.parentNode.children[3].classList.replace("opacity-1", "opacity-0");
+            break;
+        case "mouseleave":
+            this.parentNode.children[3].classList.replace("opacity-0", "opacity-1");
+            this.children[1].classList.replace("opacity-1", "opacity-0");
+        case "mouseup":
+            this.classList.remove("scale-96");
+            this.children[1].classList.replace("background-pressed", "background-hover");
+            break;
+        case "mousedown":
+            this.classList.add("scale-96");
+            this.children[1].classList.replace("background-hover", "background-pressed");
+            break;
+    }
+}
+
+/**
+ * Permet d'animer le bouton createHouse
+ * 
+ * @param {Event} event 
+ */
+const animateBtnCreateHouse = function(event) {
+    switch (event.type) {
+        case "mouseup":
+        case "mouseleave":
+            this.classList.remove("scale-96");
+            this.children[1].classList.replace("opacity-1", "opacity-0");
+            this.children[1].classList.replace("background-pressed", "background-hover");
+            break;
+        case "mousedown":
+            this.classList.add("scale-96");
+            this.children[1].classList.replace("background-hover", "background-pressed");
+            break;
+        case "mouseenter":
+            this.children[1].classList.replace("opacity-0", "opacity-1");
+            break;
+    }
+}
+
+let timeoutID = [];
+/**
+ * Permet l'animation quand on cherche une maison
+ * 
+ * @param {Event} event 
+ */
+const animateSearch = function(event) {
+    if (timeoutID.length > 0) { clearTimeout(timeoutID.shift()); }
+    timeoutID.push(setTimeout(() => {
+        timeoutID = [];
+        searchHousesGlobal.children[1].children[0].children[0].textContent = this.value;
+        searchHousesGlobal.setAttribute("href", "/houses/search?s"+this.value);
+        if (this.value == "") {
+            hideSearch();
+        } else {
+            searchGlobalContainer.classList.replace("display-none", "display-true");
+            pinHouses.classList.replace("display-true", "display-none");
+            separator.classList.replace("display-true", "display-none");
+            createContainer.classList.replace("display-true", "display-none");
+            searchHouse(this.value);
+        }
+    }, 500));
+}
+
+const hideSearch = function() {
+    searchGlobalContainer.classList.replace("display-true", "display-none");
+    pinHouses.classList.replace("display-none", "display-true");
+    separator.classList.replace("display-none", "display-true");
+    createContainer.classList.replace("display-none", "display-true");
+    for (let button of myHouses.children[1].children) { button.style.display = "flex" }
+}
+
+const searchHouse = function(value) {
+    for (let button of myHouses.children[1].children) {
+        if (!isMatch(button, value)) {
+            button.style.display = "none";
+        }
+    }
+}
+
+/**
+ * 
+ * @param {HTMLElement} button 
+ * @param {String} test 
+ * @returns 
+ */
+const isMatch = (button, test) => button.children[1].children[0].textContent.toLocaleLowerCase().includes(test.toLowerCase());
+
+//btnHouses.dispatchEvent(new Event("click"));
+
