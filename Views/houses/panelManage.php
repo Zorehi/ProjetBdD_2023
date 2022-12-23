840e3de9ea666965babe4aea3d156bfba8bb921b
Zorehi <jeremy284@hotmail.com>
2022-12-23 05:41:10 +0100

    HTML et CSS pour panelManage maison fini

--- Views/houses/panelManage.php
+++ Views/houses/panelManage.php
@@ -23,12 +23,8 @@
         <div class="scrollbar-container" id="scrollbar-12">
             <div class="scrollbar-content" data-transition="yes">
                 <div class="panel-manage-main">
-                    <a href="houses/?id=<?= $house->getId_house() ?>" class="panel-section-button" id="Home_house" data-status="unselected">
-                        <svg fill="currentColor" viewBox="0 0 20 20" width="20px" height="20px">
-                            <g fill-rule="evenodd" transform="translate(-446 -398)">
-                                <path fill-rule="nonzero" d="M457.507 411v5.55c0 .525.426.95.95.95h3.5a1.55 1.55 0 0 0 1.55-1.55v-7.507h1.271c.994 0 1.672-1.186.918-1.947l-8.772-8.14a1.361 1.361 0 0 0-1.823-.01l-8.714 8.122c-.784.699-.29 2.032.782 2.032h1.338v7.45c0 .856.695 1.55 1.55 1.55h3.5a.95.95 0 0 0 .95-.95V411h3zm-1.5-11.448 7.966 7.39h-1.216a.75.75 0 0 0-.75.75v8.258a.05.05 0 0 1-.05.05h-2.95v-5.35a1.15 1.15 0 0 0-1.15-1.15h-3.7a1.15 1.15 0 0 0-1.15 1.15V416h-2.95a.05.05 0 0 1-.05-.05v-8.2a.75.75 0 0 0-.75-.75h-1.24l7.99-7.448z"></path>
-                            </g>
-                        </svg>
+                    <a href="houses/<?= $house->getId_house() ?>" class="panel-section-button" id="Home_house" data-status="unselected">
+                        <i class="image"></i>
                         <div class="text unselectable">
                             <span class="primary">Accueil de la maison</span>
                         </div>
@@ -48,7 +44,8 @@
                         </div>
                         <div class="hover"></div>
                     </div>
-                    <a href="houses/?id=<?= $house->getId_house() ?>&page=house_aparts" class="panel-section-button" data-status="unselected">
+                    <a href="houses/<?= $house->getId_house() ?>/house_aparts" id="house_aparts" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -22px;"></i>
                         <div class="text unselectable">
                             <span class="primary">Appartements de la maison</span>
                             <span class="secondary">1 appartement</span>
@@ -67,10 +64,11 @@
                         </div>
                         <div class="hover"></div>
                     </div>
-                    <a href="houses/?id=<?= $house->getId_house() ?>&page=edit" class="panel-section-button" data-status="unselected">
+                    <a href="houses/<?= $house->getId_house() ?>/edit" id="edit_house" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -44px;"></i>
                         <div class="text unselectable">
                             <span class="primary">Paramètres de la maison</span>
-                            <span class="secondary">Gérez le nom et adresse</span>
+                            <span class="secondary">Gérez le nom et l'adresse</span>
                         </div>
                         <div class="hover"></div>
                     </a>
@@ -86,13 +84,15 @@
                         </div>
                         <div class="hover"></div>
                     </div>
-                    <a href="houses/?id=<?= $house->getId_house() ?>&page=consume" class="panel-section-button" data-status="unselected">
+                    <a href="houses/<?= $house->getId_house() ?>/insights/?section=consume" id="house_consume" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -66px;"></i>
                         <div class="text unselectable">
                             <span class="primary">Consommation</span>
                         </div>
                         <div class="hover"></div>
                     </a>
-                    <a href="houses/?id=<?= $house->getId_house() ?>&page=emit" class="panel-section-button" data-status="unselected">
+                    <a href="houses/<?= $house->getId_house() ?>/insights/?section=emit" id="house_emit" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -88px;"></i>
                         <div class="text unselectable">
                             <span class="primary">Emission</span>
                         </div>
