2d15fd010283d64f6ba949f56d800f9d7e4feeb4
Polygregoire <103888409+Polygregoire@users.noreply.github.com>
2022-12-26 17:36:21 +0100

    alors peut etre housecreate

--- Controllers/HousesController.php
+++ Controllers/HousesController.php
@@ -50,43 +50,31 @@ class HousesController extends Controller
     // Permet d'afficher la page house/create
     public function create() {
         $pageName = "Créer une maison | Projet BdD";
-
-        $this->render('/houses/create', compact('pageName'));
-    }
-
-    
-
-    // Damn la première ligne de code en PHP - MDR 
-
-    // C'est Jérémy qui écrit : ce qu'il y a dans cette fonction c'est à mettre dans la fonction create !!!
-    // Et les lignes que j'ai commenté c'est parce qu'il y a une erreur dessus donc le site web marchait plus
-    public function createHouse() {
-        $pageName = "Créer une maison | Projet BdD";
         $house = new HouseModel;
-        //$houseArray = $house->findById($house_name);
-
         // On appelle la méthode validate de la classe mère pour vérifier que les champs sont bien remplis 
        if (Form::validate($_POST, ["house_name", "isolation_degree", "eval_eco", "citizen_degree", "street", "house_number","city_name","postcode"])) {
             /*if ($houseArray)
             {
                 // La maison existe déja on verra la gestion plus tard 
             }*/
-        // On récupère les infos du formulaires dans des variables 
         $house_name = $_POST["house_name"];
+        $houseArray = $house->findById($house_name);
+        $house->hydrate($house->findById($houseArray['house_id']));
+        $house->create();
+        
+       /* 
         $isolation_degree = $_POST["isolation_degree"];
         $eval_eco = $_POST["eval_eco"];
         $citizen_degree = $_POST["citizen_degree"];
         $street = $_POST["street"];
         $house_number = $_POST["house_number"];
-        $id_city = $_POST["city_name"];
-        
-        $house->create();
-        //$houseArray = $house->findBy('house_Name'); // Celle la à vérifier
+        $id_city = $_POST["city_name"]; */
 
-        //$userArray = $user->findBy(['email' => $user->getEmail()]);
-        //$user->setId_users($userArray['id_users']);
 
+       
        }
+
+        $this->render('/houses/create', compact('pageName'));
 
     }
     
