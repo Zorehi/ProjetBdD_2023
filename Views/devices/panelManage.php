2ce73644b95a3fd03ea8282b77989c2fcf4dd5b9
Zorehi <jeremy284@hotmail.com>
2023-01-14 21:58:40 +0100

    Update

---
+++ Views/devices/panelManage.php
@@ -0,0 +1,68 @@
+<div class="panel-section panel-manage" id="manage-device">
+    <div class="panel-manage-header">
+        <div class="panel-manage-profil">
+            <img src="assets/image/house-default-min-photo.png" alt="">
+            <div class="text">
+                <span class="primary"><?= $house->getHouse_name() ?></span>
+                <span class="secondary"><?= "Type d'appareil : " ?></span>
+            </div>
+        </div>
+        <div class="panel-manage-btn-create-container">
+            <a class="panel-section-button">
+                <div class="text unselectable">
+                    <span class="primary">Allumer</span>
+                </div>
+                <div class="hover"></div>
+            </a>
+        </div>
+    </div>
+    <div class="panel-section-content">
+        <div class="scrollbar-container" id="scrollbar-manage-device">
+            <div class="scrollbar-content" data-transition="yes">
+                <div class="panel-section-separator"></div>
+                <div class="panel-manage-wrapper" data-status="shown">
+                    <div class="panel-section-button" onclick="onClickDropDown(this.parentElement)" draggable="false" ondragstart="return false;">
+                        <div class="text unselectable">
+                            <span class="secondary">Paramètres</span>
+                        </div>
+                        <div class="icon">
+                            <i class="image"></i>
+                            <div class="hover"></div>
+                        </div>
+                        <div class="hover"></div>
+                    </div>
+                </div>
+                <div class="panel-manage-wrapper" data-status="shown">
+                    <div class="panel-section-button" onclick="onClickDropDown(this.parentElement)" draggable="false" ondragstart="return false;">
+                        <div class="text unselectable">
+                            <span class="secondary">Statistiques</span>
+                        </div>
+                        <div class="icon">
+                            <i class="image"></i>
+                            <div class="hover"></div>
+                        </div>
+                        <div class="hover"></div>
+                    </div>
+                    <a href="houses/<?= $house->getId_house() ?>/insights/?section=consume" id="house_consume" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -66px;"></i>
+                        <div class="text unselectable">
+                            <span class="primary">Consommation</span>
+                        </div>
+                        <div class="hover"></div>
+                    </a>
+                    <a href="houses/<?= $house->getId_house() ?>/insights/?section=emit" id="house_emit" class="panel-section-button" data-status="unselected">
+                        <i class="image" style="background-position-y: -88px;"></i>
+                        <div class="text unselectable">
+                            <span class="primary">Emission</span>
+                        </div>
+                        <div class="hover"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="scrollbar-track"></div>
+            <div class="scrollbar-thumb" data-transition="yes" draggable="false" ondragstart="return false;">
+                <div></div>
+            </div>
+        </div>
+    </div>
+</div>
