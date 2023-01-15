d4c60c4d13a96aa90fc60978eb50322be59b3110
CyrilJack <jacques.cyril80@gmail.com>
2023-01-15 01:06:39 +0100

    Nouvelles requetes omg

--- Models/Entities/Device_typeModel.php
+++ Models/Entities/Device_typeModel.php
@@ -16,12 +16,12 @@ class Device_typeModel extends Entity
         $this->idName = "id_device_type";
     }
 
-    public function get_name_ressource($id){
-        return $this->requete( " SELECT name,id_ressource FROM ressource NATURAL JOIN consume WHERE id_device_type = $id ")->fetch();
+    public function get_name_resource($id){
+        return $this->requete( " SELECT name,id_ressource  FROM ressource NATURAL JOIN consume WHERE id_device_type = $id ")->fetchAll();
     }
 
     public function get_name_substance($id){
-        return $this->requete( " SELECT name,id_substance FROM substance NATURAL JOIN emit WHERE id_device_type = $id ")->fetch();
+        return $this->requete( " SELECT name,id_substance FROM substance NATURAL JOIN emit WHERE id_device_type = $id ")->fetchAll();
     }
 
     /**
